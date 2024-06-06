01e13d9dd3d4becbd359d0f29690dfcbba68f3cd
Major Hayden <major@redhat.com>
2024-06-06 15:42:05 -0500

    Entrypoint improvements
    Wait for the database to be fully ready before starting the app. Add
    some status messages to track what the container is doing ad it starts.
    
    Signed-off-by: Major Hayden <major@redhat.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-curl -o ./data.json https://cloudx-json-bucket.s3.amazonaws.com/raw/aws/aws.json
-poetry install
-poetry run python ./import_data.py aws
+echo "Loading data from S3..."
+curl -sLo ./data.json https://cloudx-json-bucket.s3.amazonaws.com/raw/aws/aws.json
+
+# Keep trying to load data if the database is still initializing.
+until poetry run python ./import_data.py aws
+do
+    echo "Import failed, retrying in 5 seconds..."
+    sleep 5
+done
+
 poetry run uvicorn cid.main:app --host 0.0.0.0 --port 80
