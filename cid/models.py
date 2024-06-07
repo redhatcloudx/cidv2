1f624a636518ac9402b2078ca0003a0cf6fbf163
Felix Kolwa <fkolwa@redhat.com>
2024-06-07 17:02:33 +0200

    Add Azure and Google data add import functionality add models for google and azure image types

--- cid/models.py
+++ cid/models.py
@@ -12,7 +12,7 @@ Base = declarative_base()
 class AwsImage(Base):
     __tablename__ = "aws_images"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(String, primary_key=True, index=True)
     name = Column(String, index=True)
     arch = Column(String)
     version = Column(String)
@@ -36,3 +36,41 @@ class AwsImage(Base):
     rootDeviceType = Column(String)
     sriovNetSupport = Column(String)
     deprecationTime = Column(DateTime)
+
+
+class GoogleImage(Base):
+    __tablename__ = "google_images"
+
+    id = Column(String, primary_key=True, index=True)
+    name = Column(String, index=True)
+    arch = Column(String)
+    version = Column(String)
+    creationTimestamp = Column(DateTime)
+    description = Column(String)
+    diskSizeGb = Column(Integer)
+    family = Column(String)
+    guestOsFeatures = Column(JSON)
+    kind = Column(String)
+    labelFingerprint = Column(String)
+    licenseCodes = Column(JSON)
+    licenses = Column(JSON)
+    rawDisk = Column(JSON)
+    selfLink = Column(String)
+    sourceType = Column(String)
+    status = Column(String)
+    storageLocations = Column(JSON)
+
+
+class AzureImage(Base):
+    __tablename__ = "azure_images"
+
+    id = Column(String, primary_key=True, index=True)
+    architecture = Column(String)
+    offer = Column(String)
+    publisher = Column(String)
+    sku = Column(String)
+    urn = Column(String)
+    version = Column(String)
+
+
+
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,25 @@
 #!/bin/sh
 
 echo "Loading data from S3..."
-curl -sLo ./data.json https://cloudx-json-bucket.s3.amazonaws.com/raw/aws/aws.json
+curl -sLo ./aws.json https://cloudx-json-bucket.s3.amazonaws.com/raw/aws/aws.json \
+&& curl -sLo ./azure.json https://cloudx-json-bucket.s3.amazonaws.com/raw/azure/eastus.json \
+&& curl -sLo ./google.json https://cloudx-json-bucket.s3.amazonaws.com/raw/google/global.json \
+|| { echo "Failed to download JSON files. Exiting..."; exit 1; }
 
 # Keep trying to load data if the database is still initializing.
-until poetry run python ./import_data.py aws
-do
-    echo "Import failed, retrying in 5 seconds..."
-    sleep 5
+until poetry run python ./import_data.py aws aws.json; do
+  echo "Import failed for AWS, retrying in 5 seconds..."
+  sleep 5
+done
+
+until poetry run python ./import_data.py azure azure.json; do
+  echo "Import failed for Azure, retrying in 5 seconds..."
+  sleep 5
+done
+
+until poetry run python ./import_data.py google google.json; do
+  echo "Import failed for Google, retrying in 5 seconds..."
+  sleep 5
 done
 
 poetry run uvicorn cid.main:app --host 0.0.0.0 --port 80
