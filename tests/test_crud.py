5244cb50171740ffadaddfb6b61c1c9775ed3663
Major Hayden <major@redhat.com>
2024-06-11 15:12:04 -0500

    Refactor azure/google latest imags funcs

--- tests/test_crud.py
+++ tests/test_crud.py
@@ -7,26 +7,28 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 from cid import crud
-from cid.models import AwsImage
+from cid.models import AwsImage, AzureImage, GoogleImage
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def db_engine(request):
     """yields a SQLAlchemy engine which is suppressed after the test session"""
     engine_ = create_engine("sqlite:///:memory:")
 
     # Add tables to the database.
     AwsImage.metadata.create_all(bind=engine_)
+    AzureImage.metadata.create_all(bind=engine_)
 
     yield engine_
 
     # Remove tables.
     AwsImage.metadata.drop_all(bind=engine_)
+    AzureImage.metadata.create_all(bind=engine_)
 
     engine_.dispose()
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def db_session_factory(db_engine):
     """returns a SQLAlchemy scoped session factory"""
     return scoped_session(sessionmaker(bind=db_engine))
@@ -41,6 +43,14 @@ def db_session(db_session_factory):
 
     session_.rollback()
     session_.close()
+
+
+def test_latest_aws_image_no_images(db_session_factory):
+    db = db_session_factory
+
+    result = crud.latest_aws_image(db)
+    print(result)
+    assert result == {"error": "No images found", "code": 404}
 
 
 def test_latest_aws_image(db_session_factory):
@@ -67,7 +77,6 @@ def test_latest_aws_image(db_session_factory):
     ]
 
     db.add_all(images)
-    db.commit()
 
     result = crud.latest_aws_image(db)
     assert result["name"] == "test_image_2"
@@ -87,3 +96,67 @@ def test_latest_aws_image(db_session_factory):
     result = crud.latest_aws_image(db)
     assert result["name"] == "test_image_2"
     assert result["amis"] == {"us-west-1": "ami-b", "us-west-2": "ami-c"}
+
+
+def test_latest_azure_image_no_images(db_session_factory):
+    db = db_session_factory
+
+    result = crud.latest_azure_image(db)
+    assert result == {"error": "No images found", "code": 404}
+
+
+def test_latest_azure_image(db_session_factory):
+    db = db_session_factory
+
+    images = [
+        AzureImage(
+            id="urn-a",
+            sku="sku-a",
+            offer="offer-a",
+            version="1.0",
+            urn="urn-a",
+        ),
+        AzureImage(
+            id="urn-b",
+            sku="sku-a",
+            offer="offer-a",
+            version="2.0",
+            urn="urn-b",
+        ),
+    ]
+    db.add_all(images)
+
+    result = crud.latest_azure_image(db)
+    assert result["sku"] == "sku-a"
+    assert result["version"] == "2.0"
+
+
+def test_latest_google_image_no_images(db_session_factory):
+    db = db_session_factory
+
+    result = crud.latest_google_image(db)
+    assert result == {"error": "No images found", "code": 404}
+
+
+def test_latest_google_image(db_session_factory):
+    db = db_session_factory
+
+    images = [
+        GoogleImage(
+            id="image-a",
+            name="test_image_1",
+            version="1.0",
+            creationTimestamp=datetime.strptime("2022-01-01", "%Y-%m-%d").date(),
+        ),
+        GoogleImage(
+            id="image-b",
+            name="test_image_2",
+            version="2.0",
+            creationTimestamp=datetime.strptime("2022-01-01", "%Y-%m-%d").date(),
+        ),
+    ]
+    db.add_all(images)
+
+    result = crud.latest_google_image(db)
+    assert result["name"] == "test_image_2"
+    assert result["version"] == "2.0"
