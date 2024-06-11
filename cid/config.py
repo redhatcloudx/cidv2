aa6dc4e7a74df0456d4c9c8a63f8af4aed60bcec
Major Hayden <major@redhat.com>
2024-06-11 10:42:02 -0500

    Add config for image data JSON URLs

--- cid/config.py
+++ cid/config.py
@@ -2,3 +2,9 @@
 
 # Postgres database connection string.
 DATABASE_URL = "postgresql://postgres:postgres@db:5432/test_db"
+
+# Image data for populating the database.
+IMAGE_DATA_BASE_URL = "https://cloudx-json-bucket.s3.amazonaws.com/raw"
+AWS_IMAGE_DATA = f"{IMAGE_DATA_BASE_URL}/aws/aws.json"
+AZURE_IMAGE_DATA = f"{IMAGE_DATA_BASE_URL}/azure/eastus.json"
+GCP_IMAGE_DATA = f"{IMAGE_DATA_BASE_URL}/google/global.json"
