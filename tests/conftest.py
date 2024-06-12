a8435f63af32f0bc85b5827d0a0a4f68439ac100
Major Hayden <major@redhat.com>
2024-06-12 10:29:57 -0500

    Simplify database tests
    We can remove lots of extra code since we're re-using the existing
    database connection engine/session in our tests. 🎉
    
    Signed-off-by: Major Hayden <major@redhat.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,42 +1,25 @@
 """Shared items for testing."""
 
 import pytest
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
 
-from cid.models import AwsImage, AzureImage
-
-
-@pytest.fixture(scope="function")
-def db_engine(request):
-    """yields a SQLAlchemy engine which is suppressed after the test session"""
-    engine_ = create_engine("sqlite:///:memory:")
-
-    # Add tables to the database.
-    AwsImage.metadata.create_all(bind=engine_)
-    AzureImage.metadata.create_all(bind=engine_)
-
-    yield engine_
-
-    # Remove tables.
-    AwsImage.metadata.drop_all(bind=engine_)
-    AzureImage.metadata.create_all(bind=engine_)
-
-    engine_.dispose()
+from cid.database import SessionLocal, engine
+from cid.models import AwsImage, AzureImage, GoogleImage
 
 
 @pytest.fixture(scope="function")
-def db_session_factory(db_engine):
-    """returns a SQLAlchemy scoped session factory"""
-    return scoped_session(sessionmaker(bind=db_engine))
+def db():
+    """Yield a sqlalchemy database connection after creating the tables."""
+    db = SessionLocal()
 
+    AwsImage.metadata.create_all(bind=engine)
+    AzureImage.metadata.create_all(bind=engine)
+    GoogleImage.metadata.create_all(bind=engine)
 
-@pytest.fixture(scope="function")
-def db_session(db_session_factory):
-    """yields a SQLAlchemy connection which is rollbacked after the test"""
-    session_ = db_session_factory()
+    yield db
 
-    yield session_
+    AwsImage.metadata.drop_all(bind=engine)
+    AzureImage.metadata.drop_all(bind=engine)
+    GoogleImage.metadata.drop_all(bind=engine)
 
-    session_.rollback()
-    session_.close()
+    db.rollback()
+    db.close()
