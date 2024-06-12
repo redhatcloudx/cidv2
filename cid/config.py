685f8724234ff339df297188b3556b1fa20628d8
Major Hayden <major@redhat.com>
2024-06-12 10:17:28 -0500

    Choose DB string based on env
    Signed-off-by: Major Hayden <major@redhat.com>

--- cid/config.py
+++ cid/config.py
@@ -1,7 +1,16 @@
 """Centralized configuration for the project."""
 
-# Postgres database connection string.
-DATABASE_URL = "postgresql://postgres:postgres@db:5432/test_db"
+import os
+
+# Get the current environment (default to testing if not set).
+ENVIRONMENT = os.getenv("ENVIRONMENT", "testing")
+
+# Database connection string.
+DATABASE_URLS = {
+    "production": "postgresql://postgres:postgres@db:5432/test_db",
+    "testing": "sqlite:///:memory:",
+}
+DATABASE_URL = DATABASE_URLS[ENVIRONMENT]
 
 # Image data for populating the database.
 IMAGE_DATA_BASE_URL = "https://cloudx-json-bucket.s3.amazonaws.com/raw"
