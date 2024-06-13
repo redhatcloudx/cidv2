148acb6ffed3c24f117dc3a62914ca9409fd0d66
Major Hayden <major@redhat.com>
2024-06-13 09:42:55 -0500

    Exclude BETA images from latest query

--- cid/crud.py
+++ cid/crud.py
@@ -30,7 +30,10 @@ def latest_aws_image(db: Session) -> dict[str, Any]:
     """Get the latest RHEL image on AWS."""
     # Find the image with the highest version number and the latest date.
     latest_image = (
-        db.query(AwsImage).order_by(desc(AwsImage.version), desc(AwsImage.date)).first()
+        db.query(AwsImage)
+        .filter(AwsImage.name.notlike("%BETA%"))
+        .order_by(desc(AwsImage.version), desc(AwsImage.date))
+        .first()
     )
 
     if latest_image is None:
